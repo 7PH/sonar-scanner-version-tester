2d3d012bc9d8c108f35d483ee07828f6a40a17b9
7PH <benjamin.raymond@sonarsource.com>
2024-06-24 16:00:50 +0200

    Fix support for prepare.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-cp sonar-project.properties src/
+cp sonar-project.properties prepare.sh src/
 
 cd src
 
+# Run prepare.sh if it exists
+if [ -f "prepare.sh" ]; then
+    chmod +x prepare.sh
+    ./prepare.sh
+fi
+
+# Run scanner
 sonar-scanner \
     -Dsonar.organization=$SONAR_ORGANIZATION \
     -Dsonar.projectKey=$SONAR_PROJECT_KEY \
